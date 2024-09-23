5f6e59caa1f682897440f14094a0dcaf406b41ce
SvGaming <sven.kovac1@outlook.com>
2024-09-23 21:36:49 +0200

    add files

---
+++ PoseidonPlugin/src/main/java/org/example/poseidonplugin/PoseidonPlugin.java
@@ -0,0 +1,13 @@
+package org.example.poseidonplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PoseidonPlugin extends JavaPlugin {
+    public void onEnable() {
+        getServer().getLogger().info("Hello from the Project Poseidon example project!");
+    }
+
+    public void onDisable() {
+
+    }
+}
